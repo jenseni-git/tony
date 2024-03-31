6d349d87cd55a157e4d7b559a14bbd3749956ddb
jenseni-git <jenseniain@gmail.com>
2024-03-31 16:39:29 +1000

    bot.go Indentation formatting to AddHandler()

--- framework/bot.go
+++ framework/bot.go
@@ -24,32 +24,33 @@ func NewBot(token string, serverId string) (*Bot, error) {
 }
 
 func (b *Bot) DefineModerationRules(rules ...ActionableRule) {
-	b.discord.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		if m.Author.ID == s.State.User.ID {
-			return
-		}
+	b.discord.AddHandler(
+		func(s *discordgo.Session, m *discordgo.MessageCreate) {
+			if m.Author.ID == s.State.User.ID {
+				return
+			}
 
-		// Get Channel Name from Message
-		channel, err := s.State.Channel(m.ChannelID)
-		if err != nil {
-			return
-		}
+			// Get Channel Name from Message
+			channel, err := s.State.Channel(m.ChannelID)
+			if err != nil {
+				return
+			}
 
-		// Test a regex match for the channel name against the rule
-		for _, rule := range rules {
-			if match, _ := regexp.Match(rule.Channel, []byte(channel.Name)); match {
+			// Test a regex match for the channel name against the rule
+			for _, rule := range rules {
+				if match, _ := regexp.Match(rule.Channel, []byte(channel.Name)); match {
 
-				// Test the rule
-				if err := rule.Rule.Test(m.Content); err != nil {
-					rule.Rule.Action(&Context{
-						Message: m.Message,
-						Session: s,
-					}, err)
+					// Test the rule
+					if err := rule.Rule.Test(m.Content); err != nil {
+						rule.Rule.Action(&Context{
+							Message: m.Message,
+							Session: s,
+						}, err)
+					}
 				}
 			}
-		}
-
-	})
+		},
+	)
 }
 
 func (b *Bot) Run() error {
